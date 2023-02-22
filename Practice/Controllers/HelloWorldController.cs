b324b65cc729be041693e14015049e7db7eb2bf0
allen <tim_que@xinwang.com.tw>
2023-02-22 12:22:52 +0800

    Project Init

---
+++ Practice/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["mystring1"] = "I am ViewData !";
+            ViewBag.mystring2 = "I am ViewBag !";
+            //ViewData[]
+            return View("index1");
+        }
+        public string Step1(string name)
+        {
+            return $"I am {name}";
+        }
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Practice.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<PracticeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PracticeContext") ?? throw new InvalidOperationException("Connection string 'PracticeContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Products}/{action=Index}/{id?}");
+
+app.Run();
